0ce704ad551bec1ab071616b64b2c5e59c349678
Guilherme Furlan <guilherme.bazanelifurlan@gmail.com>
2024-09-12 11:11:26 -0300

    inicia o menu da aplicação

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-// hello world
-
-// let mensagem = "olá, mundo!"
-// console.log(mensagem);
-// console.log(mensagem);
-// console.log(mensagem);
-// console.log(mensagem);
-
-//  arrays, objetos
-
-// let metas = ["gui", "alo"]
-// console.log(metas[1] + ", " + metas[0] + "!!!");
+// Acessa o node_modules, dentro da pasta @inquirer, a pasta prompts e extraindo
+const {select} = require('@inquirer/prompts')
 
 
-// let meta = {
-//     value: "ler um livro por mês",
-//     checked: false
-// }
-// console.log(meta.value)
-
-// // named function // arrow function
-
-// function criarMeta() {}
-// const criarMeta = () => {}
-
-
-const start = () => {
+const start = async () => {
     
     while(true) {
-        let opcao = 'cadastrar'
+        
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
         switch(opcao) {
             case 'cadastrar':
                 console.log('vamos cadastrar')
@@ -36,6 +28,7 @@ const start = () => {
                 console.log('vamos listar')
                 break
             case 'sair':
+                console.log('Até a próxima!')
                 return
         }
     }

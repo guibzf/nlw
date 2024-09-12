d849bb2f3885b83b5d48b3532cb56f3fb9c547fd
Guilherme Furlan <guilherme.bazanelifurlan@gmail.com>
2024-09-12 16:08:13 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -47,6 +47,27 @@ const listarMetas = async () => {
     console.log("Meta(s) concluída(s)!")
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log("Não existem metas realizadas :(")
+        return
+    }
+    
+    await select({
+        message: "Metas realizadas",
+        choices: [...realizadas]
+    })
+}
+
+
+
+
+
+
 const start = async () => {
     
     while(true) {
@@ -63,6 +84,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -76,6 +101,9 @@ const start = async () => {
                 break
             case 'listar':
                 await listarMetas()
+                break
+            case 'realizadas':
+                await metasRealizadas()
                 break
             case 'sair':
                 console.log('Até a próxima!')
